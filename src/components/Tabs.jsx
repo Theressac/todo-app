5380ac1a9a6727b90e2b1b4154f3155301144339
Theressac <theressa.coujan@gmail.com>
2024-10-31 06:03:01 +0400

    todo-app
    React JS Todo App with the localstorage

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,17 +1,33 @@
 export function Tabs(props) {
-   const { todos } = props
+  const { todos, selectedTab, setSelectedTab } = props;
   const tabs = ["All", "Open", "Completed"];
   return (
     <nav className="tab-container">
       {tabs.map((tab, indexkey) => {
+        const numOfTasks =
+          tab === "All"
+            ? todos.length
+            : tab === "Open"
+            ? todos.filter((val) => !val.complete).length
+            : todos.filter((val) => val.complete).length;
+
         return (
-          <button key={indexkey} className="tab-button">
+          <button
+            onClick={() => {
+              setSelectedTab(tab);
+            }}
+            key={indexkey}
+            className={
+              "tab-button " + (tab === selectedTab ? "tab-selected" : " ")
+            }
+          >
             <h4>
-              {tab} <span>(0)</span>
+              {tab} <span>{numOfTasks}</span>
             </h4>
           </button>
         );
       })}
+      <hr />
     </nav>
   );
 }
